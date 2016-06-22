e7aa232918b0a9f4ed4ff917a323c53d65451eb2
Mostafa Asadi <mostafaasadi73@gmail.com>
2016-06-22 20:01:12 +0430

    installer file

---
+++ sidc_installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# SIDC installer 
+# usl : https://github.com/mostafaasadi/sidc/
+clear
+echo -e "\n\n\t\t installing sidc ... "
+echo -e "\t\t\t please wait...\n"
+sudo cp -f sidc /usr/bin/
+sudo chmod +x sidc
+echo -e "\n\t\t SIDC installed successfully !
+	\t\tfor help run sidc -h \n "
+
