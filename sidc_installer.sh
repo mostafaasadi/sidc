d716ca0b0fbcce3d99cebf6e61ae2a9a524c3b10
Mostafa Asadi <mostafaasadi73@gmail.com>
2016-06-25 05:09:48 +0430

    Update sidc_installer.sh
    add GUI

--- sidc_installer.sh
+++ sidc_installer.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# SIDC installer 
-# usl : https://github.com/mostafaasadi/sidc/
+# SIDC installer
+# url : https://github.com/mostafaasadi/sidc/
 clear
 echo -e "\n\n\t\t installing sidc ... "
 echo -e "\t\t\t please wait...\n"
 sudo cp -f sidc /usr/bin/
+echo -e "\n\t\t* sidc file copied to /user/bin"
+sleep 0.5
 sudo chmod +x sidc
-echo -e "\n\t\t SIDC installed successfully !
+echo -e "\n\t\t* sidc file got permission"
+sleep 0.5
+mkdir $HOME/.icons 2>/dev/null
+cp -f sidc.png $HOME/.icons/
+echo -e "\n\t\t* sidc icon copied"
+sleep 0.5
+echo -e "\n\n\t\t SIDC installed successfully !
 	\t\tfor help run sidc -h \n "
-
